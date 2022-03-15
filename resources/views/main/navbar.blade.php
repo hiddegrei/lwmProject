83e9b3e9bd26c3114c973f4dd0c6f083e3e02f40
Mitchel0058 <goed0058@hz.nl>
2022-03-15 14:08:02 +0100

    fixed saif el pizza, again

--- resources/views/main/navbar.blade.php
+++ resources/views/main/navbar.blade.php
@@ -56,15 +56,14 @@
                 <a  style="color:white;" href="/" class="navbar-item px-4 text-decoration-none">
                     Quick guide
                 </a>
-                <!-- <a><img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/2560px-Flag_of_the_United_States.svg.png" style="" ></a> -->
-
 
             </div>
 
         </div>
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/2560px-Flag_of_the_United_States.svg.png" id="americanFlag">
-    <div class="text-light">Saif el pizza</div>
     </div>
+
+    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/2560px-Flag_of_the_United_States.svg.png" id="americanFlag">
+    <div class="text-light">Saif el pizza</div>
 </nav>
 
 
