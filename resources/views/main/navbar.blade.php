4d123988f40b26a0eb47f18d07dbd3432b73c925
Mitchel0058 <goed0058@hz.nl>
2022-03-16 10:50:55 +0100

    fixed layout

--- resources/views/main/navbar.blade.php
+++ resources/views/main/navbar.blade.php
@@ -1,8 +1,10 @@
-<nav class="navbar is-primary  has-text-white border-bottom border-primary fixed-top" style="background-color: #00385A;">
+<nav class="navbar is-primary  has-text-white border-bottom border-primary fixed-top"
+     style="background-color: #00385A;">
     <div class="container">
         <div class="navbar-brand">
             <a href="/" class="navbar-item">
-            <img src="https://lwmdev.service-now.com/f1580754dbf8b700a797298a48961940.iix" alt="Lambwaston img" class="img-fluid position-absolute start-0" style="width: 110px">
+                <img src="https://lwmdev.service-now.com/f1580754dbf8b700a797298a48961940.iix" alt="Lambwaston img"
+                     class="img-fluid position-absolute start-0" style="width: 110px">
             </a>
             <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
                 <span aria-hidden="true"></span>
@@ -13,88 +15,90 @@
 
         <div class="navbar-menu " id="navMenu">
 
-        <div class="navbar-menu " id="navMenu">
+            <div class="navbar-menu " id="navMenu">
 
-            <div class="navbar-start">
-            <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">To Dos</button>
-                 <div class="dropdown-content">
-                    <a href="#">Eat</a>
-                    <a href="#">Pet dog</a>
-                    <a href="#">Potato</a>
-                </div>
-                </div>
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">To Dos</button>
+                        <div class="dropdown-content">
+                            <a href="#">Eat</a>
+                            <a href="#">Pet dog</a>
+                            <a href="#">Potato</a>
+                        </div>
+                    </div>
 
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">My Tickets</button>
-                 <div class="dropdown-content">
-                    <a href="#">Train ticket</a>
-                    <a href="#">Car ticket</a>
-                </div>
-                </div>
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">My Tickets</button>
+                        <div class="dropdown-content">
+                            <a href="#">Train ticket</a>
+                            <a href="#">Car ticket</a>
+                        </div>
+                    </div>
 
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Knowledge</button>
-                 <div class="dropdown-content">
-                    <a href="#">Book of all</a>
-                    <a href="#">Book of nothing</a>
-                    <a href="#">Book of Book</a>
-                </div>
-                </div>
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Knowledge</button>
+                        <div class="dropdown-content">
+                            <a href="#">Book of all</a>
+                            <a href="#">Book of nothing</a>
+                            <a href="#">Book of Book</a>
+                        </div>
+                    </div>
 
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Services</button>
-                 <div class="dropdown-content">
-                    <a href="#">Food</a>
-                    <a href="#">Sunflower oil</a>
-                    <a href="#">Handyman</a>
-                </div>
-                </div>
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Services</button>
+                        <div class="dropdown-content">
+                            <a href="#">Food</a>
+                            <a href="#">Sunflower oil</a>
+                            <a href="#">Handyman</a>
+                        </div>
+                    </div>
 
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">System Status</button>
-                 <div class="dropdown-content">
-                    <a href="#">Business support</a>
-                    <a href="#">Facilities</a>
-                    <a href="#">Finance</a>
-                    <a href="#">Home office</a>
-                    <a href="#">HR & Payroll</a>
-                    <a href="#">IT services</a>
-                    <a href="#">Masterdata</a>
-                </div>
-                </div>
-
-
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">GRC</button>
-                 <div class="dropdown-content">
-                    <a href="#">Wat is GRC?</a>
-                </div>
-                </div>
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">System Status</button>
+                        <div class="dropdown-content">
+                            <a href="#">Business support</a>
+                            <a href="#">Facilities</a>
+                            <a href="#">Finance</a>
+                            <a href="#">Home office</a>
+                            <a href="#">HR & Payroll</a>
+                            <a href="#">IT services</a>
+                            <a href="#">Masterdata</a>
+                        </div>
+                    </div>
 
 
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Cart</button>
-                 <div class="dropdown-content">
-                    <a href="#">Items</a>
-                     <a href="#">Buy</a>
-                </div>
-                </div>
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">GRC</button>
+                        <div class="dropdown-content">
+                            <a href="#">Wat is GRC?</a>
+                        </div>
+                    </div>
 
-                <div class="dropdown">
-                <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Quick Guide</button>
-                 <div class="dropdown-content">
-                     <a href="#">You have no quick guides</a>
-                </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Cart</button>
+                        <div class="dropdown-content">
+                            <a href="#">Items</a>
+                            <a href="#">Buy</a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn navbar-item px-4 text-decoration-none navHover">Quick Guide</button>
+                        <div class="dropdown-content">
+                            <a href="#">You have no quick guides</a>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
-
         </div>
-    </div>
 
-    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/2560px-Flag_of_the_United_States.svg.png" id="americanFlag">
-    <div class="text-light">Saif el pizza</div>
+        <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/2560px-Flag_of_the_United_States.svg.png"
+            id="americanFlag">
+        <div class="text-light">Saif el pizza</div>
 </nav>
 
 
