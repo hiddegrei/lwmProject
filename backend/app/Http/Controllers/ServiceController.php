89a61732a3e95ce4b95a73d33deb4ab9bdf393b6
Mitchel0058 <goed0058@hz.nl>
2022-04-12 10:31:47 +0200

    deleted old no longer used files

--- backend/app/Http/Controllers/ServiceController.php
+++ backend/app/Http/Controllers/ServiceController.php
@@ -19,7 +19,7 @@ class ServiceController extends Controller
     public function index($servicetype){
 
         $data = Service::where("servicetype",$servicetype)->orderBy('created_at', 'desc')->get();
-        
+
 
         return $data;
 
@@ -28,17 +28,17 @@ class ServiceController extends Controller
     public function getAll(){
         $top10ids = ServiceTrack::orderBy('count', 'desc')->limit(10)->get();
         $data=[];
-       
+
 
         for ($x = 0; $x < count($top10ids); $x++) {
             $data[$x]=Service::where("id",$top10ids[$x]->service_id)->get()[0];
-           
+
           }
         // return Service::all();
         return $data;
     }
 
-  
+
     public function create(Request $request){
         $data = request()->validate([
             'title'=> 'required',
@@ -46,8 +46,8 @@ class ServiceController extends Controller
             'servicetype'=> 'required',
             'dropdowns'=>'',
             'questions'=>''
-           
-           
+
+
 
         ]);
 //    $dropdowns=json_decode(request('dropdowns'));
@@ -57,7 +57,7 @@ class ServiceController extends Controller
             'opened_for'=>'required',
         'opened_by'=>'required',
         'needs_approval_from'=>'',
-        
+
         ]);
 
         // $newData=array_merge($data,['dropdowns'=>$dropdowns,'questions'=>$questions]);
@@ -70,26 +70,26 @@ class ServiceController extends Controller
             $item = Service::create($newData);
 
         }
-         
+
         //  $imagePath=\Image::make($request->get('image'))->store('services','public');
-        
-        
+
+
         //  $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,100);
         //     $image->save();
 
-       
 
-       
+
+
             $item->checks()->create($data2);
 
-            
-       
-        
+
+
+
         $item->serviceTrack()->create([
             'service_id'=>$item->id,
             'count'=>0
         ]);
-       
+
         $set=array_merge($data,$data2);
         return $set;
 
@@ -111,15 +111,15 @@ class ServiceController extends Controller
             'service'=>$serviceid,
             'checks'=> $serviceChecks
         ];
-        // dd($data["checks"][0]->needs_approval_from);
+//         dd($data["checks"][0]->needs_approval_from);
          $data["checks"][0]->needs_approval_from_name=User::findOrFail(intval($data["checks"][0]->needs_approval_from));
 
         $serviceid->serviceTrack()->update([
             'count'=> $serviceid->serviceTrack()->get('count')[0]->count +1
         ]);
-        
-       
+
+
          return $data ;
- 
+
     }
 }
